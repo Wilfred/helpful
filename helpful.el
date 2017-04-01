8fe0c499afc1a7058f580f9e7f49aee5da39837c
Wilfred Hughes <me@wilfred.me.uk>
2017-04-01 19:37:25 +0100

    Skeleton file

---
+++ helpful.el
@@ -0,0 +1,38 @@
+;;; helpful.el --- a better *help* buffer            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Wilfred Hughes
+
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Keywords: help, lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defun helpful (symbol)
+  "Show Help for SYMBOL."
+  (interactive
+   (list (completing-read "Symbol: " obarray
+                          nil nil nil nil
+                          (symbol-name (symbol-at-point)))))
+  )
+
+
+
+(provide 'helpful)
+;;; helpful.el ends here
