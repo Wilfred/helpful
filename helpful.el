7ec9bfc110e905abdceaac9016e12aa2d849700c
Wilfred Hughes <me@wilfred.me.uk>
2017-04-01 19:44:34 +0100

    Basic rendering

--- helpful.el
+++ helpful.el
@@ -24,15 +24,32 @@
 
 ;;; Code:
 
+(defvar-local helpful--sym nil)
+
+(defun helpful--buffer (symbol)
+  "Return a buffer to show help for SYMBOL in."
+  (let ((buf (get-buffer-create
+              (format "*helpful: %s" symbol))))
+    (with-current-buffer buf
+      (setq helpful--sym symbol))))
+
+(defun helpful--update (fn-symbol)
+  "Update the current *insight* buffer to the latest state of FN-SYMBOL."
+  (let ((inhibit-read-only t)
+        (start-pos (point)))
+    (erase-buffer)
+    (insert
+     (format "Symbol: %s\n" fn-symbol))
+    (goto-char start-pos)))
+
 (defun helpful (symbol)
   "Show Help for SYMBOL."
   (interactive
    (list (completing-read "Symbol: " obarray
                           nil nil nil nil
                           (symbol-name (symbol-at-point)))))
-  )
-
-
+  (switch-to-buffer (helpful--buffer symbol))
+  (helpful--update symbol))
 
 (provide 'helpful)
 ;;; helpful.el ends here
