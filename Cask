b714a404c522c2ddf6b401235c177acdab2b242c
Wilfred Hughes <me@wilfred.me.uk>
2017-04-02 09:42:28 +0100

    Define a cask package

---
+++ Cask
@@ -0,0 +1,7 @@
+(source melpa)
+
+(package-file "helpful.el")
+
+(development
+ (depends-on "f")
+ (depends-on "ert-runner"))
--- helpful.el
+++ helpful.el
@@ -4,6 +4,7 @@
 
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Keywords: help, lisp
+;; Version: 0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
