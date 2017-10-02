7afd19bde2a2402e817ea3ad50378b66316e0e7d
Narendra Joshi <narendraj9@gmail.com>
2017-10-02 21:39:39 +0530

    Fix grammar of logged line

--- helpful.el
+++ helpful.el
@@ -748,7 +748,7 @@ For example, \"(some-func FOO &optional BAR)\"."
       (user-error "No command is bound to %s"
                   (key-description key-sequence)))
     (unless (commandp sym)
-      (user-error "%s is bound to symbol - %s, that is not a command"
+      (user-error "%s is bound to symbol %s which is not a command"
                   (key-description key-sequence)
                   sym))
     (pop-to-buffer (helpful--buffer sym t))
