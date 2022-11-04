4f9cc0d03adb810c000f013ddbe34687de07c9d0
ksqsf <login@ksqsf.moe>
2022-11-04 22:07:00 +0800

    Remove use of (point) as a generalized variable
    This use has been made obsolete since Emacs 29.

--- helpful.el
+++ helpful.el
@@ -1760,7 +1760,7 @@ POSITION-HEADS takes the form ((123 (defun foo)) (456 (defun bar)))."
       (with-temp-buffer
         (declare-function cl--generic-describe "cl-generic" (function))
         (cl--generic-describe func)
-        (setf (point) (point-min))
+        (goto-char (point-min))
         (when (re-search-forward "^Implementations:$" nil t)
           (setq content (buffer-substring (point) (point-max)))))
       (when content
